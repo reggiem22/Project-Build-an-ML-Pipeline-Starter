f39137014ef689c8612b01458ab7ea5fe84ce8cf
reggiem22 <reginald7great@gmail.com>
2025-04-07 10:21:12 -0500

    work on basic cleaning

--- main.py
+++ main.py
@@ -54,7 +54,18 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+             _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                entry_point="main",
+                parameters={
+                    "input_artifact": "sample.csv:latest",  
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "cleaned_data",
+                    "output_description": "Cleaned dataset after basic preprocessing",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
 
         if "data_check" in active_steps:
             ##################
